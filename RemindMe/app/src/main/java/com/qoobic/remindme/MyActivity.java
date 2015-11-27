ceb6c70ca55c9d13e47a1a197442059cd0fa6e9a
ArmenPetrosyan <armenak.petrosyan92@gmail.com>
2015-11-28 01:23:02 +0400

    Toolbar and search

---
+++ RemindMe/app/src/main/java/com/qoobic/remindme/MyActivity.java
@@ -0,0 +1,29 @@
+package com.qoobic.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class MyActivity extends Activity {
+    private Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar=(Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
+    }
+}
